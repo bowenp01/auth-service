df9613dde6e47959160db13c2f4e3ad36cbeb3c5
Paul Bowen <bowenp01@gmail.com>
2021-06-27 15:56:42 +0100

    removed console.log statements

--- database/Dummy/authorise.js
+++ database/Dummy/authorise.js
@@ -8,7 +8,6 @@ const User = require("../../models/user");
 function authoriseLocal(un, pw, callback) {
   // Populate the user object to pass to the callback method (roles are empty for now)
   let user = new User(un, 'Dummy user', 'Dummy email', "Dummy department", []);
-  console.log(user);
   callback(true, user);
 }
 
